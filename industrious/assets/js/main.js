76c64866f7423ad30a6f53d7a90d45d184b60234
Alfen <alfen.nishanth.s1@ibm.com>
2024-09-26 19:37:52 +0530

    reset testimonials

--- industrious/assets/js/main.js
+++ industrious/assets/js/main.js
@@ -78,30 +78,3 @@ const observer = new IntersectionObserver((entries) => {
 }, { threshold: 0.5 }); // Start when 50% of the section is in view
 
 observer.observe(document.querySelector('#counter-section'));
-
-
-jQuery(document).ready(function($) {
-	"use strict";
-	//  TESTIMONIALS CAROUSEL HOOK
-	$('#customers-testimonials').owlCarousel({
-		loop: true,
-		center: true,
-		items: 3,
-		margin: 0,
-		autoplay: true,
-		dots:true,
-		autoplayTimeout: 2500,
-		smartSpeed: 450,
-		responsive: {
-		  0: {
-			items: 1
-		  },
-		  768: {
-			items: 2
-		  },
-		  1170: {
-			items: 3
-		  }
-		}
-	});
-});
